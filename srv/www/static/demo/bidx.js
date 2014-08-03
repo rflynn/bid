12c7f56b53b582c7873b7a5eb08db4c0a66d3e30
Ryan Flynn <parseerror@gmail.com>
2014-08-03 17:03:52 -0400

    refactor most bidx js into separate bidx.js file

---
+++ srv/www/static/demo/bidx.js
@@ -0,0 +1,55 @@
+
+var bidx = {
+
+    init: function()
+    {
+        return true;
+    },
+
+    bid_url: function(price)
+    {
+        return 'http://bidx.co/bid/?price='+price+'&t='+Date.now();
+    },
+
+    on_bid_response: function(span, msg)
+    {
+        if (msg.hasOwnProperty('id')) {
+            var price = msg['price'];
+            var vendor = msg['id'];
+            var link = 'http://www.' + msg['id'] + '/';
+            console.log(span, price, msg, link);
+            $('span#'+span+' span.price').text('$'+Math.round(price));
+            $('span#'+span+' span.site a').attr('href', link);
+            $('span#'+span+' span.site a').text(vendor);
+        }
+    },
+
+    bid: function(span)
+    {
+        var price = $('span#'+span+' span.price').text().substr(1);
+        var jqxhr = $.ajax({
+            url: bidx.bid_url(price),
+            dataType: 'json',
+            timeout: 3000
+        })
+        .done(function(msg) { bidx.on_bid_response(span, msg); })
+        .fail(function() { console.log("error"); })
+        .always(function() { $('span#'+span).show('slow'); });
+    },
+
+    run: function()
+    {
+        var biddable = document.getElementsByClassName('bidx');
+        for (var i = 0; i < biddable.length; i++) {
+            var span = biddable[i].attributes["id"].value;
+            console.log(span);
+            $('span#'+span).hide() // TODO: remove dependence on jQuery
+            setTimeout(function(span) {
+                return function() {
+                    bidx.bid(span)
+                }
+            }(span), 500*i);
+        }
+    }
+};
+
