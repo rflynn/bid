24c17ece843b0ed57aa33fcc91b1682bd20b5293
Ryan Flynn <parseerror@gmail.com>
2014-08-03 00:09:18 -0400

    get nginx -> uwsgi -> flask configs working

--- srv/www.py
+++ srv/www.py
@@ -1,4 +1,13 @@
+# vim: set ts=4 et:
 
-def application(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return ['web']
+from datetime import datetime
+from flask import Flask, request
+
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Hello from Flask! (%s)' % datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
